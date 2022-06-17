d0d1919f026e01c4bc1e297053c0addd4e0503a3
Julia Nory <noryj605@gmail.com>
2022-06-16 23:27:25 -0300

    corrigindo arquivo do banco de dados

--- 2. Banco de dados/ddl.sql
+++ 2. Banco de dados/ddl.sql
@@ -1,27 +1,27 @@
-CREATE DATABASE maquiagemDB;
+CREATE DATABASE IF NOT EXISTS maquiagemDB;
 USE maquiagemDB;
 
-CREATE TABLE tb_usuario(
+CREATE TABLE IF NOT EXISTS tb_usuario(
 	id_usuario 		INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    ds_email 		VARCHAR(60),
-    ds_senha 		VARCHAR(20),
-    nm_usuario 		VARCHAR(100),
-    dt_nascimento 	DATE
+   ds_email 		VARCHAR(60),
+   ds_senha 		VARCHAR(20),
+   nm_usuario 		VARCHAR(100),
+   dt_nascimento 	DATE
 );
 
-CREATE TABLE tb_maquiagem(
+CREATE TABLE IF NOT EXISTS tb_maquiagem(
 	id_maquiagem 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nm_maquiagem 	VARCHAR(100),
-    vl_preco 		FLOAT
+   nm_maquiagem 	VARCHAR(100),
+   vl_maquiagem 	FLOAT
 );
 
-CREATE TABLE tb_agenda(
-	id_agenda 		    INT NOT NULL PRIMARY KEY auto_increment,
-    id_usuario 		    INT NOT NULL,
-    id_maquiagem 		INT NOT NULL,
-    dt_agendamento 	    DATE,
-    hr_agendamento 	    TIME,
+CREATE TABLE IF NOT EXISTS tb_agenda(
+	id_agenda 		INT NOT NULL PRIMARY KEY auto_increment,
+   id_usuario 		INT NOT NULL,
+   id_maquiagem 	INT NOT NULL,
+   dt_agendamento DATE,
+   hr_agendamento TIME,
     
-    FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_usuario),
-    FOREIGN KEY (id_maquiagem) REFERENCES tb_maquiagem (id_maquiagem)
+   FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_usuario),
+   FOREIGN KEY (id_maquiagem) REFERENCES tb_maquiagem (id_maquiagem)
 );
