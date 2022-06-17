9391a7cac91f2fb0e3a8e404d1a99ac23784e176
Julia Nory <noryj605@gmail.com>
2022-06-17 02:04:09 -0300

    rota de maquiagem funcionando com sucesso

--- api/src/controller/maquiagemController.js
+++ api/src/controller/maquiagemController.js
@@ -1,14 +1,48 @@
-import { criarMaquiagem } from "../repository/maquiagemRepository.js";
+import { alterarMaquiagem, criarMaquiagem, filtrarMaquiagem } from "../repository/maquiagemRepository.js";
 
 import { Router } from "express";
 const server = Router();
-
 
 server.post('/maquiagem', async(req, resp) => {
     try{
 
         const resposta = await criarMaquiagem(req.body);
         resp.send(resposta);
+
+    }catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        });
+    }
+});
+
+server.get('/maquiagem/:id', async(req, resp) => {
+    try{
+        
+        const { id } = req.params;
+        const resposta = await filtrarMaquiagem(id);
+        resp.send(resposta);
+
+    }catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        });
+    }
+});
+
+server.put('/maquiagem/:id', async(req, resp) => {
+    try{
+        
+        const { id } = req.params;
+        const maquiagem = req.body;
+
+        const resposta = await alterarMaquiagem(id, maquiagem);
+        if(resposta != 1)
+            throw new Error("Error: Maquiagem não pode ser atualizada");
+
+        resp.status(200).send({
+            message: 'Maquiagem atualizada com sucesso'
+        });
 
     }catch (err) {
         resp.status(400).send({
--- api/src/repository/maquiagemRepository.js
+++ api/src/repository/maquiagemRepository.js
@@ -11,3 +11,26 @@ export async function criarMaquiagem(maquiagem){
 
     return maquiagem;
 }
+
+export async function filtrarMaquiagem(id){
+
+    const comando = 
+        `SELECT * FROM tb_maquiagem 
+          WHERE id_maquiagem = ?`;
+
+    const [resposta] = await con.query(comando, [id]);
+    return resposta[0];
+}
+
+export async function alterarMaquiagem(id, maquiagem){
+
+    const comando = 
+        `UPDATE tb_maquiagem
+            SET nm_maquiagem = ?,
+                vl_maquiagem = ?
+          WHERE id_maquiagem = ?`;
+
+    const [resposta] = await con.query(comando, [maquiagem.nome, maquiagem.valor, id]);
+    return resposta.affectedRows;
+
+}
