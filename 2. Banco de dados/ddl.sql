7da243a46fd5b4c0a3080bfd64d06eb1cbd9ef74
Julia Nory <noryj605@gmail.com>
2022-06-16 23:28:31 -0300

    ajustando alinhamento do arquivo de ddl.sql

--- 2. Banco de dados/ddl.sql
+++ 2. Banco de dados/ddl.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS maquiagemDB;
 USE maquiagemDB;
 
 CREATE TABLE IF NOT EXISTS tb_usuario(
-	id_usuario 		INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   id_usuario 	    INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
    ds_email 		VARCHAR(60),
    ds_senha 		VARCHAR(20),
    nm_usuario 		VARCHAR(100),
@@ -10,17 +10,17 @@ CREATE TABLE IF NOT EXISTS tb_usuario(
 );
 
 CREATE TABLE IF NOT EXISTS tb_maquiagem(
-	id_maquiagem 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   id_maquiagem 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
    nm_maquiagem 	VARCHAR(100),
    vl_maquiagem 	FLOAT
 );
 
 CREATE TABLE IF NOT EXISTS tb_agenda(
-	id_agenda 		INT NOT NULL PRIMARY KEY auto_increment,
+   id_agenda 		INT NOT NULL PRIMARY KEY auto_increment,
    id_usuario 		INT NOT NULL,
    id_maquiagem 	INT NOT NULL,
-   dt_agendamento DATE,
-   hr_agendamento TIME,
+   dt_agendamento   DATE,
+   hr_agendamento   TIME,
     
    FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_usuario),
    FOREIGN KEY (id_maquiagem) REFERENCES tb_maquiagem (id_maquiagem)
