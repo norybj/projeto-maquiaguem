e7c94c8677d025ccdad4453ae1ff0b498dc60397
Julia Nory <noryj605@gmail.com>
2022-06-20 04:05:35 -0300

    adicionando mais métodos para a rota de usuario

--- api/src/controller/usuarioController.js
+++ api/src/controller/usuarioController.js
@@ -1,4 +1,4 @@
-import { login, cadastrarUsuario, filtrarUsuario } from "../repository/usuarioRepository.js";
+import { login, cadastrarUsuario, alterarUsuario, filtrarUsuarioPorId, filtrarUsuarioPorEmail } from "../repository/usuarioRepository.js";
 
 import { Router } from "express";
 const server = Router();
@@ -35,12 +35,34 @@ server.post('/usuario', async(req, resp) => {
     }
 });
 
+server.put('/usuario/:id', async(req, resp) => {
+    try{
+
+        const { id } = req.params;
+        const usuario = req.body;
+
+        const resposta = await alterarUsuario(usuario, id);
+        if(resposta != 1)
+            throw new Error("Error: Maquiagem não pode ser atualizada");
+
+        resp.status(200).send({
+            message: 'Usuário atualizado com sucesso'
+        });
+
+    }catch (err) {
+
+        resp.status(400).send({
+            erro: err.message
+        });
+
+    }
+});
+
 server.get('/usuario/:id', async(req, resp) => {
     try{
 
         const { id } = req.params;
-        const resposta = await filtrarUsuario(id);
-
+        const resposta = await filtrarUsuarioPorId(id);
         resp.send(resposta);
 
     }catch (err) {
@@ -50,5 +72,21 @@ server.get('/usuario/:id', async(req, resp) => {
         });
 
     }
-})
+});
+
+server.get('/usuario/email/:email', async(req, resp) => {
+    try{
+
+        const { email } = req.params;
+        const resposta = await filtrarUsuarioPorEmail(email);
+        resp.send(resposta);
+
+    }catch (err) {
+
+        resp.status(400).send({
+            erro: err.message
+        });
+
+    }
+});
 export default server;
--- api/src/repository/usuarioRepository.js
+++ api/src/repository/usuarioRepository.js
@@ -27,12 +27,34 @@ export async function cadastrarUsuario(usuario){
 
 }
 
-export async function filtrarUsuario(id){
+export async function alterarUsuario(usuario, id){
 
     const comando = 
-        `SELECT * FROM tb_usuario WHERE id_usuario = ?`;
+        `UPDATE tb_usuario 
+            SET ds_email      = ?, 
+                ds_senha      = ?, 
+                nm_usuario    = ?, 
+                dt_nascimento = ?
+            WHERE id_usuario  = ?;`;
+
+    const [linhas] = await con.query(comando, [usuario.email, usuario.senha, usuario.nome, usuario.nascimento, id]);
+    return linhas.affectedRows;
+
+}
+
+export async function filtrarUsuarioPorId(id){
+
+    const comando = `SELECT * FROM tb_usuario WHERE id_usuario = ?`;
 
     const [resposta] = await con.query(comando, [id]);
+    return resposta[0];
+
+}
+
+export async function filtrarUsuarioPorEmail(email){
+
+    const comando = `SELECT * FROM tb_usuario WHERE ds_email = ?`;
+    const [resposta] = await con.query(comando, [email]);
     return resposta[0];
 
 }
