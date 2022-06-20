597a143531927baa213b27d7815454b43689a35b
Julia Nory <noryj605@gmail.com>
2022-06-20 04:09:37 -0300

    criando componente footer

---
+++ site/src/components/footer/index.js
@@ -0,0 +1,17 @@
+import './index.scss';
+
+export default function Index(){
+    return (
+        <footer className="comp-footer">
+                <div className="footer-image">
+                    <img src="/images/whatsapp.png" alt="Logo Redes sociais"></img>
+                    <img src="/images/facebook.png" alt="Logo Redes sociais"></img>
+                    <img src="/images/instagram.png"alt="Logo Redes sociais"></img>
+                </div>
+                <hr/>
+                <div className="footer-logo">
+                    <img src="/images/logo.png" alt = "Logo empresa"></img>
+                </div>
+        </footer>
+    );
+}
